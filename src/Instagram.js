f93a8a0cb196d4aa99d75a655ace94fc201691a4
mojtaba-borzu <mojtaba.borzu69@gmail.com>
2021-07-13 01:00:16 +0430

    changed

--- src/Instagram.js
+++ src/Instagram.js
@@ -16,10 +16,11 @@ function Instagram () {
           <div className=" flex justify-center w-3/5">
             <div className="bg-red-200"><ScrollUser /></div>
           </div>
-
           <div className="flex justify-center w-2/5">
             <div className="bg-green-200">
-              <ViewDetails />
+              <ViewDetails className="h-20 bg-pink-300" />
+              <div className="h-20 bg-blue-200">text</div>
+              <div className="h-40 bg-yellow-200">followers</div>
               <div className="bg-blue-200 content-end h-20">footer</div>
             </div>
           </div>
